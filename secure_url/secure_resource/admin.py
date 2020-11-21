c4c39f25a8cdd54ff28f828dd5092446bb685ed4
Bartek Krzys <b.krzys@clearcode.cc>
2020-11-21 13:28:46 +0100

    Display SecureFile and SecureUrl in django admin

--- secure_url/secure_resource/admin.py
+++ secure_url/secure_resource/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from secure_resource.models import SecureFile, SecureUrl
 
-# Register your models here.
+
+@admin.register(SecureFile)
+class SecureFileAdminModel(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SecureUrl)
+class SecureUrlAdminModel(admin.ModelAdmin):
+    pass
